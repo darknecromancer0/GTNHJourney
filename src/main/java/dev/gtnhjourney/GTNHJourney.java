f5413cd17defa92a0dfaf983d54d6369d758ae81
darknecromancer0 <127566984+darknecromancer0@users.noreply.github.com>
2026-08-25 05:40:09 +0300

    feat: register pickup observer on Forge event bus

--- src/main/java/dev/gtnhjourney/GTNHJourney.java
+++ src/main/java/dev/gtnhjourney/GTNHJourney.java
@@ -1,5 +1,7 @@
 package dev.gtnhjourney;
 
+import net.minecraftforge.common.MinecraftForge;
+
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -46,6 +48,7 @@ public final class GTNHJourney {
         FMLCommonHandler.instance()
             .bus()
             .register(inventoryTracker);
+        MinecraftForge.EVENT_BUS.register(inventoryTracker);
         FMLCommonHandler.instance()
             .bus()
             .register(new ServerRequestQueue(RESEARCH));
