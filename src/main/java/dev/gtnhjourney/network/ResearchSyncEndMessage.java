c60c4f3113317c2928a872774a0b7f053deaa767
darknecromancer0 <127566984+darknecromancer0@users.noreply.github.com>
2026-08-26 08:56:07 +0300

    feat: finish N activity sync with research sync

--- src/main/java/dev/gtnhjourney/network/ResearchSyncEndMessage.java
+++ src/main/java/dev/gtnhjourney/network/ResearchSyncEndMessage.java
@@ -3,6 +3,7 @@ package dev.gtnhjourney.network;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
+import dev.gtnhjourney.client.ClientActivityMirror;
 import dev.gtnhjourney.client.ClientNetworkQueue;
 import dev.gtnhjourney.client.ClientStackMirror;
 import io.netty.buffer.ByteBuf;
@@ -34,6 +35,7 @@ public final class ResearchSyncEndMessage implements IMessage {
                 @Override
                 public void run() {
                     ClientStackMirror.finish(epoch);
+                    ClientActivityMirror.finish(epoch, ClientStackMirror.snapshotKeysInResearchOrder());
                 }
             });
             return null;
