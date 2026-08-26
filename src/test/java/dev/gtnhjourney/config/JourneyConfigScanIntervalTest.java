7126c6f7d9ed11df829a1958232039078ed83f78
darknecromancer0 <127566984+darknecromancer0@users.noreply.github.com>
2026-08-27 01:04:42 +0300

    test: isolate five tick scan interval policy

--- src/test/java/dev/gtnhjourney/config/JourneyConfigScanIntervalTest.java
+++ src/test/java/dev/gtnhjourney/config/JourneyConfigScanIntervalTest.java
@@ -2,39 +2,17 @@ package dev.gtnhjourney.config;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.File;
-import java.io.FileWriter;
-
 import org.junit.jupiter.api.Test;
 
 public class JourneyConfigScanIntervalTest {
 
     @Test
-    public void newConfigDefaultsToFiveTicks() throws Exception {
-        File file = File.createTempFile("gtnhjourney-config-default", ".cfg");
-        if (!file.delete()) throw new IllegalStateException("could not prepare temp config path");
-        try {
-            JourneyConfig.load(file);
-            assertEquals(5, JourneyConfig.inventoryScanIntervalTicks());
-        } finally {
-            file.delete();
-        }
-    }
-
-    @Test
-    public void legacyTwoTickConfigIsRaisedToFiveTicks() throws Exception {
-        File file = File.createTempFile("gtnhjourney-config-legacy", ".cfg");
-        try {
-            FileWriter writer = new FileWriter(file);
-            try {
-                writer.write("research {\n    I:inventoryScanIntervalTicks=2\n}\n");
-            } finally {
-                writer.close();
-            }
-            JourneyConfig.load(file);
-            assertEquals(5, JourneyConfig.inventoryScanIntervalTicks());
-        } finally {
-            file.delete();
-        }
+    public void defaultAndLegacyIntervalsUseFiveTickFloor() {
+        assertEquals(5, JourneyConfig.DEFAULT_INVENTORY_SCAN_INTERVAL_TICKS);
+        assertEquals(5, JourneyConfig.normalizeInventoryScanIntervalTicks(1));
+        assertEquals(5, JourneyConfig.normalizeInventoryScanIntervalTicks(2));
+        assertEquals(5, JourneyConfig.normalizeInventoryScanIntervalTicks(5));
+        assertEquals(20, JourneyConfig.normalizeInventoryScanIntervalTicks(20));
+        assertEquals(40, JourneyConfig.normalizeInventoryScanIntervalTicks(40));
     }
 }
