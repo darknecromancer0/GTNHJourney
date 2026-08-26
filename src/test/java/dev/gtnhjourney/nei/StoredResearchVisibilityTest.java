9829ded977d06b2486b7669a52c33570dcbdd0d4
darknecromancer0 <127566984+darknecromancer0@users.noreply.github.com>
2026-08-26 08:44:10 +0300

    test: update stored N visibility contract

--- src/test/java/dev/gtnhjourney/nei/StoredResearchVisibilityTest.java
+++ src/test/java/dev/gtnhjourney/nei/StoredResearchVisibilityTest.java
@@ -3,6 +3,7 @@ package dev.gtnhjourney.nei;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -13,7 +14,7 @@ public class StoredResearchVisibilityTest {
 
     @Test
     public void researchedPanelIncludesStoredDrillAndFilledCellKeysNewestFirstWithoutNativeNeiPredicate() {
-        ResearchKey drillBase = new ResearchKey("IC2:itemToolDrill", 26, "");
+        ResearchKey drillBase = new ResearchKey("IC2:itemToolDrill", 27, "");
         ResearchKey drillFull = new ResearchKey("IC2:itemToolDrill", 1, "10{6:charge=6:30000.0d;}");
         ResearchKey filledCell = new ResearchKey(
             "IC2:itemFluidCell",
@@ -28,8 +29,8 @@ public class StoredResearchVisibilityTest {
     }
 
     @Test
-    public void newestPanelAppliesOnlyItsLimitAfterStoredChronologyOrdering() {
-        ResearchKey drillBase = new ResearchKey("IC2:itemToolDrill", 26, "");
+    public void newestPanelShowsOnlyTheLastStoredResearchState() {
+        ResearchKey drillBase = new ResearchKey("IC2:itemToolDrill", 27, "");
         ResearchKey drillFull = new ResearchKey("IC2:itemToolDrill", 1, "10{6:charge=6:30000.0d;}");
         ResearchKey filledCell = new ResearchKey(
             "IC2:itemFluidCell",
@@ -37,10 +38,10 @@ public class StoredResearchVisibilityTest {
             "10{5:Fluid=10{6:Amount=3:1000;9:FluidName=8:\"molten.orundum\";};}");
 
         assertEquals(
-            Arrays.asList(filledCell, drillFull),
+            Collections.singletonList(filledCell),
             JourneyPanelSnapshot.keys(
                 Arrays.asList(drillBase, drillFull, filledCell),
                 JourneyViewState.Mode.NEWEST,
-                2));
+                64));
     }
 }
