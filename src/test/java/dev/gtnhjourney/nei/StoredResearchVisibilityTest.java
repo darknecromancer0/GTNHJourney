0d4d2d19369d6e231eb4b4d7d1bbba7b0a862216
darknecromancer0 <127566984+darknecromancer0@users.noreply.github.com>
2026-08-26 06:34:24 +0300

    test: preserve stored drill and fluid visibility

---
+++ src/test/java/dev/gtnhjourney/nei/StoredResearchVisibilityTest.java
@@ -0,0 +1,46 @@
+package dev.gtnhjourney.nei;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import dev.gtnhjourney.research.ResearchKey;
+
+public class StoredResearchVisibilityTest {
+
+    @Test
+    public void researchedPanelIncludesStoredDrillAndFilledCellKeysNewestFirstWithoutNativeNeiPredicate() {
+        ResearchKey drillBase = new ResearchKey("IC2:itemToolDrill", 26, "");
+        ResearchKey drillFull = new ResearchKey("IC2:itemToolDrill", 1, "10{6:charge=6:30000.0d;}");
+        ResearchKey filledCell = new ResearchKey(
+            "IC2:itemFluidCell",
+            0,
+            "10{5:Fluid=10{6:Amount=3:1000;9:FluidName=8:\"molten.orundum\";};}");
+
+        List<ResearchKey> oldestFirst = Arrays.asList(drillBase, drillFull, filledCell);
+
+        assertEquals(
+            Arrays.asList(filledCell, drillFull, drillBase),
+            JourneyPanelSnapshot.keys(oldestFirst, JourneyViewState.Mode.RESEARCHED, 64));
+    }
+
+    @Test
+    public void newestPanelAppliesOnlyItsLimitAfterStoredChronologyOrdering() {
+        ResearchKey drillBase = new ResearchKey("IC2:itemToolDrill", 26, "");
+        ResearchKey drillFull = new ResearchKey("IC2:itemToolDrill", 1, "10{6:charge=6:30000.0d;}");
+        ResearchKey filledCell = new ResearchKey(
+            "IC2:itemFluidCell",
+            0,
+            "10{5:Fluid=10{6:Amount=3:1000;9:FluidName=8:\"molten.orundum\";};}");
+
+        assertEquals(
+            Arrays.asList(filledCell, drillFull),
+            JourneyPanelSnapshot.keys(
+                Arrays.asList(drillBase, drillFull, filledCell),
+                JourneyViewState.Mode.NEWEST,
+                2));
+    }
+}
