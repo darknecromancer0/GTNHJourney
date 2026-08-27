15a6b80bedbcea17ac66cd757bcb72cdd6248e4a
darknecromancer0 <127566984+darknecromancer0@users.noreply.github.com>
2026-08-27 05:16:49 +0300

    test: reject unregistered GT tool meta

---
+++ src/test/java/dev/gtnhjourney/minecraft/GtToolStatePolicyTest.java
@@ -0,0 +1,29 @@
+package dev.gtnhjourney.minecraft;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import net.minecraft.item.ItemStack;
+
+import org.junit.jupiter.api.Test;
+
+import gregtech.api.items.MetaGeneratedTool;
+
+public class GtToolStatePolicyTest {
+
+    @Test
+    public void registeredGeneratedToolMetaIsVerified() {
+        MetaGeneratedTool tool = new MetaGeneratedTool();
+        tool.mToolStats.put(Short.valueOf((short) 10), new Object());
+
+        assertTrue(GtToolStatePolicy.isVerifiedTool(new ItemStack(tool, 1, 10)));
+    }
+
+    @Test
+    public void unregisteredLegacyGeneratedToolMetaIsRejected() {
+        MetaGeneratedTool tool = new MetaGeneratedTool();
+        tool.mToolStats.put(Short.valueOf((short) 10), new Object());
+
+        assertFalse(GtToolStatePolicy.isVerifiedTool(new ItemStack(tool, 1, 0)));
+    }
+}
