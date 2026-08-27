19bbf92a5fbe1c5ffcb73291b790253aeb724ca9
darknecromancer0 <127566984+darknecromancer0@users.noreply.github.com>
2026-08-27 04:32:25 +0300

    feat: register live Journey command hints

--- src/main/java/dev/gtnhjourney/ClientProxy.java
+++ src/main/java/dev/gtnhjourney/ClientProxy.java
@@ -1,9 +1,12 @@
 package dev.gtnhjourney;
 
+import net.minecraftforge.common.MinecraftForge;
+
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import dev.gtnhjourney.client.ClientConnectionTracker;
 import dev.gtnhjourney.client.ClientNetworkQueue;
+import dev.gtnhjourney.client.JourneyCommandHintOverlay;
 import dev.gtnhjourney.nei.JourneyCreativeInventorySafety;
 
 /** Client lifecycle that must exist before NEI config discovery or the first server research sync. */
@@ -20,5 +23,6 @@ public final class ClientProxy extends CommonProxy {
         FMLCommonHandler.instance()
             .bus()
             .register(new JourneyCreativeInventorySafety());
+        MinecraftForge.EVENT_BUS.register(new JourneyCommandHintOverlay());
     }
 }
