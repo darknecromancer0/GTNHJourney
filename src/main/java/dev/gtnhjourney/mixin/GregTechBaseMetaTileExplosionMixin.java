f5d1cd1ca6f064231a9db99d6a7737533d9dd206
darknecromancer0 <127566984+darknecromancer0@users.noreply.github.com>
2026-08-31 13:27:08 +0300

    fix: mark optional GregTech machine target pseudo

--- src/main/java/dev/gtnhjourney/mixin/GregTechBaseMetaTileExplosionMixin.java
+++ src/main/java/dev/gtnhjourney/mixin/GregTechBaseMetaTileExplosionMixin.java
@@ -1,6 +1,7 @@
 package dev.gtnhjourney.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -8,6 +9,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import dev.gtnhjourney.config.JourneyConfig;
 
 /** Prevents GregTech machines and multiblock hatches from destroying themselves when Journey explosions are off. */
+@Pseudo
 @Mixin(targets = "gregtech.api.metatileentity.BaseMetaTileEntity", remap = false)
 public abstract class GregTechBaseMetaTileExplosionMixin {
 
