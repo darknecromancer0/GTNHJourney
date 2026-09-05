114e369c87d40751cfdc907e504df614ba75deff
darknecromancer0 <127566984+darknecromancer0@users.noreply.github.com>
2026-09-05 06:22:48 +0300

    test: define directional favourite gestures

--- src/test/java/dev/gtnhjourney/nei/JourneyRetrieveClickPolicyTest.java
+++ src/test/java/dev/gtnhjourney/nei/JourneyRetrieveClickPolicyTest.java
@@ -28,13 +28,22 @@ public class JourneyRetrieveClickPolicyTest {
     }
 
     @Test
-    public void altLeftClickInterceptsOnlyFavouriteToggleViews() {
-        assertTrue(JourneyRetrieveClickPolicy.shouldToggleFavourite(JourneyViewState.Mode.RESEARCHED, 0, true));
-        assertTrue(JourneyRetrieveClickPolicy.shouldToggleFavourite(JourneyViewState.Mode.NEWEST, 0, true));
-        assertTrue(JourneyRetrieveClickPolicy.shouldToggleFavourite(JourneyViewState.Mode.FAVOURITE, 0, true));
-        assertFalse(JourneyRetrieveClickPolicy.shouldToggleFavourite(JourneyViewState.Mode.CREATIVE, 0, true));
-        assertFalse(JourneyRetrieveClickPolicy.shouldToggleFavourite(JourneyViewState.Mode.RESEARCHED, 0, false));
-        assertFalse(JourneyRetrieveClickPolicy.shouldToggleFavourite(JourneyViewState.Mode.RESEARCHED, 1, true));
+    public void altLeftAddsOnlyFromJAndN() {
+        assertTrue(JourneyRetrieveClickPolicy.shouldAddFavourite(JourneyViewState.Mode.RESEARCHED, 0, true));
+        assertTrue(JourneyRetrieveClickPolicy.shouldAddFavourite(JourneyViewState.Mode.NEWEST, 0, true));
+        assertFalse(JourneyRetrieveClickPolicy.shouldAddFavourite(JourneyViewState.Mode.FAVOURITE, 0, true));
+        assertFalse(JourneyRetrieveClickPolicy.shouldAddFavourite(JourneyViewState.Mode.CREATIVE, 0, true));
+        assertFalse(JourneyRetrieveClickPolicy.shouldAddFavourite(JourneyViewState.Mode.RESEARCHED, 1, true));
+        assertFalse(JourneyRetrieveClickPolicy.shouldAddFavourite(JourneyViewState.Mode.RESEARCHED, 0, false));
+    }
+
+    @Test
+    public void altRightRemovesOnlyFromF() {
+        assertTrue(JourneyRetrieveClickPolicy.shouldRemoveFavourite(JourneyViewState.Mode.FAVOURITE, 1, true));
+        assertFalse(JourneyRetrieveClickPolicy.shouldRemoveFavourite(JourneyViewState.Mode.RESEARCHED, 1, true));
+        assertFalse(JourneyRetrieveClickPolicy.shouldRemoveFavourite(JourneyViewState.Mode.NEWEST, 1, true));
+        assertFalse(JourneyRetrieveClickPolicy.shouldRemoveFavourite(JourneyViewState.Mode.FAVOURITE, 0, true));
+        assertFalse(JourneyRetrieveClickPolicy.shouldRemoveFavourite(JourneyViewState.Mode.FAVOURITE, 1, false));
     }
 
     @Test
