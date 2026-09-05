4fac10974b05af542bcb27844037686cb4cfa632
darknecromancer0 <127566984+darknecromancer0@users.noreply.github.com>
2026-09-05 10:47:16 +0300

    test: remove obsolete N view click expectations

--- src/test/java/dev/gtnhjourney/nei/JourneyRetrieveClickPolicyTest.java
+++ src/test/java/dev/gtnhjourney/nei/JourneyRetrieveClickPolicyTest.java
@@ -28,11 +28,11 @@ public class JourneyRetrieveClickPolicyTest {
     }
 
     @Test
-    public void altLeftAddsOnlyFromJAndN() {
+    public void altLeftAddsOnlyFromJ() {
         assertTrue(JourneyRetrieveClickPolicy.shouldAddFavourite(JourneyViewState.Mode.RESEARCHED, 0, true));
-        assertTrue(JourneyRetrieveClickPolicy.shouldAddFavourite(JourneyViewState.Mode.NEWEST, 0, true));
         assertFalse(JourneyRetrieveClickPolicy.shouldAddFavourite(JourneyViewState.Mode.FAVOURITE, 0, true));
         assertFalse(JourneyRetrieveClickPolicy.shouldAddFavourite(JourneyViewState.Mode.CREATIVE, 0, true));
+        assertFalse(JourneyRetrieveClickPolicy.shouldAddFavourite(JourneyViewState.Mode.ALL, 0, true));
         assertFalse(JourneyRetrieveClickPolicy.shouldAddFavourite(JourneyViewState.Mode.RESEARCHED, 1, true));
         assertFalse(JourneyRetrieveClickPolicy.shouldAddFavourite(JourneyViewState.Mode.RESEARCHED, 0, false));
     }
@@ -41,7 +41,7 @@ public class JourneyRetrieveClickPolicyTest {
     public void altRightRemovesOnlyFromF() {
         assertTrue(JourneyRetrieveClickPolicy.shouldRemoveFavourite(JourneyViewState.Mode.FAVOURITE, 1, true));
         assertFalse(JourneyRetrieveClickPolicy.shouldRemoveFavourite(JourneyViewState.Mode.RESEARCHED, 1, true));
-        assertFalse(JourneyRetrieveClickPolicy.shouldRemoveFavourite(JourneyViewState.Mode.NEWEST, 1, true));
+        assertFalse(JourneyRetrieveClickPolicy.shouldRemoveFavourite(JourneyViewState.Mode.CREATIVE, 1, true));
         assertFalse(JourneyRetrieveClickPolicy.shouldRemoveFavourite(JourneyViewState.Mode.FAVOURITE, 0, true));
         assertFalse(JourneyRetrieveClickPolicy.shouldRemoveFavourite(JourneyViewState.Mode.FAVOURITE, 1, false));
     }
